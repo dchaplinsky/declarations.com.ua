c845b5d0224977ae101a836b2a61e20164f6af07
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-03-03 22:06:31 +0200

    More uggliness to the mapping problem, but fine grain control of a mapping

--- declarations_site/catalog/elastic_models.py
+++ declarations_site/catalog/elastic_models.py
@@ -1,14 +1,61 @@
 from elasticsearch_dsl import DocType, Mapping
+from elasticsearch_dsl.connections import connections
 
-declarations_mapping = Mapping('declarations')
-declarations_mapping.field("general", 'object')
-declarations_mapping["general"].property(
-    'full_name_suggest', 'completion',
-    preserve_separators=False
-)
+
+class MyUglyMapping(object):
+    doc_type = "declarations"
+    properties = {
+        doc_type: {
+            "properties": {
+                "general": {
+                    "type": "object",
+
+                    "properties": {
+                        "full_name_suggest": {
+                            "type": "completion",
+                            "preserve_separators": False
+                        },
+
+                        "family_raw": {
+                            "type": "string"
+                        },
+
+                        "family": {
+                            "properties": {
+                                "name": {"type": "string"},
+                            },
+                            "type": "object"
+                        },
+
+                        "post": {
+                            "properties": {
+                                "post": {"type": "string"},
+                                "office": {"type": "string"},
+                            },
+                            "type": "object"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    def save(self, index, using="default"):
+        # TODO: analyzers, ...
+        es = connections.get_connection(using)
+        if not es.indices.exists(index=index):
+            es.indices.create(
+                index=index, body={'mappings': self.properties}
+            )
+        else:
+            es.indices.put_mapping(
+                index=index,
+                doc_type=self.doc_type,
+                body=self.properties
+            )
 
 
 class Declaration(DocType):
     class Meta:
         index = 'catalog'
-        mapping = declarations_mapping
+        mapping = Mapping('declarations')
