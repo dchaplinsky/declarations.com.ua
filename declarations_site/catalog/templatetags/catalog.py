696f1645f274747b1aed11a51e907305c3b5bbc0
Artem Hluvchynskyi <excieve@gmail.com>
2015-03-29 06:05:25 +0300

    Consistent naming for updated_querystring

--- declarations_site/catalog/templatetags/catalog.py
+++ declarations_site/catalog/templatetags/catalog.py
@@ -4,9 +4,9 @@ register = Library()
 
 
 @register.global_function
-def updated_queryset(request, params):
-    """Updates current queryset with a given dict of params, removing existing
-    occurrences of such params. Returns a urlencoded querystring."""
+def updated_querystring(request, params):
+    """Updates current querystring with a given dict of params, removing
+    existing occurrences of such params. Returns a urlencoded querystring."""
     original_params = request.GET.copy()
     for key in params:
         if key in original_params:
