7c0165923cfd1004d5e622fcd59745e871315d7d
Artem Hluvchynskyi <excieve@gmail.com>
2015-04-01 01:00:29 +0300

    Implements basic JSON API, fixes #6
    - Implements "hybrid_response" decorator for view functions that returns
      either HTML or JSON representation of the data
    - Implements an ugly recursive serializer with a hint method for paginator
    - Adds support for "search all" (useful for API)
    - Small improvements

--- declarations_site/declarations_site/urls.py
+++ declarations_site/declarations_site/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns(
     url(r'^search$', 'catalog.views.search', name='search'),
     url(r'^declaration/(?P<declaration_id>\d+)$', 'catalog.views.details',
         name='details'),
-    url(r'^region/$', 'catalog.views.regions_home', name='regions_home',),
+    url(r'^region$', 'catalog.views.regions_home', name='regions_home',),
 
     url(r'^region/(?P<region_name>.+)/(?P<office_name>.+)$', 'catalog.views.region_office',
         name='region_office'),
