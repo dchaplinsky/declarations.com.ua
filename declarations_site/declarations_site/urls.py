e92089550b62c073e07e70380034ec7614cebfdc
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-04-04 22:06:33 +0300

    GA snippet moved to head

--- declarations_site/declarations_site/urls.py
+++ declarations_site/declarations_site/urls.py
@@ -5,19 +5,22 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'catalog.views.home', name='home'),
     url(r'^ajax/suggest$', 'catalog.views.suggest', name='suggest'),
-    url(r'^about/$', TemplateView.as_view(template_name='about.jinja'),
+    url(r'^about$', TemplateView.as_view(template_name='about.jinja'),
         name="about"),
 
     url(r'^search$', 'catalog.views.search', name='search'),
     url(r'^declaration/(?P<declaration_id>\d+)$', 'catalog.views.details',
         name='details'),
-    url(r'^region$', 'catalog.views.regions_home', name='regions_home',),
 
-    url(r'^region/(?P<region_name>[^\/]+)/(?P<office_name>.+)$', 'catalog.views.region_office',
-        name='region_office'),
+    url(r'^region$', 'catalog.views.regions_home', name='regions_home',),
     url(r'^region/(?P<region_name>.+)$', 'catalog.views.region',
         name='region'),
+    url(r'^region/(?P<region_name>[^\/]+)/(?P<office_name>.+)$',
+        'catalog.views.region_office', name='region_office'),
 
     url(r'^office/(?P<office_name>.+)$', 'catalog.views.office',
         name='office'),
+
+    url(r'^sitemap.xml$', 'catalog.views.sitemap',
+        name='sitemap'),
 )
