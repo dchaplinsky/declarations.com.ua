9c88a1d4f7cf445fd64db34853d5dff8eb61d82f
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-05-04 21:41:53 +0300

    Media settings

--- declarations_site/declarations_site/settings.py
+++ declarations_site/declarations_site/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
