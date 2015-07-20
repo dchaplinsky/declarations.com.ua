21b129b09cce1e5cf26653bc0660e0b3630a10cc
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-07-20 12:10:44 +0300

    Misc cleanup

--- declarations_site/declarations_site/settings.py
+++ declarations_site/declarations_site/settings.py
@@ -13,7 +13,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
