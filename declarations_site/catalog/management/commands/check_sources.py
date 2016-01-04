8abf0d945dcaf0cf11bca7ce889db2c7162b46ce
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2016-01-04 02:34:50 +0200

    Scripts to export declarations to CSV and check if declaration sources exists

---
+++ declarations_site/catalog/management/commands/check_sources.py
@@ -0,0 +1,53 @@
+import os.path
+from django.core.management.base import BaseCommand
+from urllib.parse import unquote, urlsplit
+from catalog.elastic_models import Declaration
+
+
+class Command(BaseCommand):
+    help = 'Checks if we have all sources of the declarations'
+
+    def add_arguments(self, parser):
+        parser.add_argument('destination')
+
+    def check_url(self, url, destination):
+        path = unquote(urlsplit(url).path)
+        return os.path.exists(path.replace("/static/", destination))
+
+    def handle(self, *args, **options):
+        all_decls = Declaration.search().query('match_all').scan()
+        urls_in_list = []
+        on_unshred_it = 0
+        on_static = 0
+        on_chesno = 0
+        no_url_no_id = 0
+        not_found = 0
+        duplicates = 0
+
+        for decl in all_decls:
+            url = decl["declaration"]["url"]
+            if not url:
+                no_url_no_id += 1
+                continue
+
+            if url in urls_in_list:
+                duplicates += 1
+                print(url)
+                continue
+
+            if "unshred.it" in url.lower():
+                on_unshred_it += 1
+                if not self.check_url(url, options["destination"]):
+                    print(url)
+                    not_found += 1
+
+            elif "static.declarations.com.ua" in url.lower():
+                on_static += 1
+                if not self.check_url(url, options["destination"]):
+                    print(url)
+                    not_found += 1
+            else:
+                on_chesno += 1
+
+        print(on_unshred_it, on_static, on_chesno, no_url_no_id)
+        print(not_found, duplicates)
---
+++ declarations_site/catalog/management/commands/export_to_csv.py
@@ -0,0 +1,97 @@
+import sys
+import dpath.util
+from csv import DictWriter
+import argparse
+from django.core.management.base import BaseCommand
+from catalog.elastic_models import Declaration
+
+
+class FilterException(Exception):
+    pass
+
+
+class Command(BaseCommand):
+    help = 'Checks if we have all sources of the declarations'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--field', nargs='*', default=["declaration.url"])
+        parser.add_argument(
+            '--from', default=0, type=int)
+        parser.add_argument(
+            '--to', default=None, type=int)
+        parser.add_argument(
+            '--outfile', nargs='?', type=argparse.FileType('w'),
+            default=sys.stdout)
+
+    def apply_operation(self, res, operation):
+        def floatify(val):
+            if val is None:
+                val = ""
+
+            return float(val.replace(",", ".") or 0)
+
+        chunks = operation.split(":")
+        flt = chunks[0]
+
+        if flt == "sum":
+            return sum(map(floatify, res))
+        elif flt == "count":
+            return len(res)
+        elif flt == "count_nonempty":
+            return len(list(filter(None, res)))
+        elif flt == "join":
+            if len(chunks) != 2:
+                raise FilterException(
+                    "filter %s requires exactly 1 param" % flt)
+
+            return chunks[1].join(res)
+        else:
+            raise FilterException("Unknown filter %s" % flt)
+
+    def fetch_field(self, doc, expr):
+        chunks = expr.split("|", 2)
+        path = chunks[0]
+        operation = chunks[1] if len(chunks) > 1 else ""
+
+        if not path:
+            return ""
+
+        try:
+            res = dpath.util.values(doc, path, separator='.')
+        except KeyError:
+            res = []
+
+        if operation:
+            res = self.apply_operation(res, operation)
+
+        if isinstance(res, (list, tuple)):
+            return ", ".join(map(str, res))
+        else:
+            return res
+
+    def handle(self, *args, **options):
+        all_decls = Declaration.search().query('match_all')
+        if options["to"] is not None:
+            all_decls = all_decls[options["from"]:options["to"]].execute()
+        elif options["from"]:
+            all_decls = all_decls[options["from"]:].execute()
+        else:
+            all_decls = all_decls.scan()
+
+        w = DictWriter(
+            options["outfile"], fieldnames=["_id"] + options["field"])
+
+        w.writeheader()
+
+        for decl in all_decls:
+            decl_dict = decl.to_dict()
+
+            row = {
+                field: self.fetch_field(decl_dict, field)
+                for field in options["field"]
+            }
+
+            row["_id"] = decl.meta.id
+
+            w.writerow(row)
