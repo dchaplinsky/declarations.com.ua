870ad863fcff45fba0f055bbf23902961f63513e
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2016-01-05 01:34:17 +0200

    New filter to classify offices

--- declarations_site/catalog/management/commands/export_to_csv.py
+++ declarations_site/catalog/management/commands/export_to_csv.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import dpath.util
 from csv import DictWriter
@@ -13,6 +14,36 @@ class FilterException(Exception):
 class Command(BaseCommand):
     help = 'Checks if we have all sources of the declarations'
 
+    office_list = {
+        "[Пп]резидента?": "офіс президента",
+        " суд": "суд",
+        "Верховна Рада": "парламент",
+        " ?[пП]рокуратура": "прокуратура",
+        " юстиції": "юстиція",
+        "ДАІ": "міліція",
+        "КМУ": "кабмін",
+        " служба": "держслужба",
+        " інспекція": "держслужба",
+        " агентство": "держслужба",
+        "Державне управління справами": "офіс президента",
+        " комітет": "держслужба",
+        "Збройні сили": "безпека",
+        "Кабінет міністрів": "кабмін",
+        "МВС": "міліція",
+        "Міністерство ": "міністерство",
+        " Мінкульту": "міністерство",
+        " комісія": "держкомісія",
+        "Національний банк": "Нацбанк",
+        " ?адміністрація": "адміністрація",
+        "Обласна рада": "облрада",
+        "Районна [^ ]+ рада": "райрада",
+        "Служба безпеки": "безпека",
+        "управління державного майна": "фонд",
+        "Фонд ": "фонд",
+        "[Вв]нутрішніх": "міліція",
+        'міська рада': 'міська рада'
+    }
+
     def add_arguments(self, parser):
         parser.add_argument(
             '--field', nargs='*', default=["declaration.url"])
@@ -23,6 +54,13 @@ class Command(BaseCommand):
         parser.add_argument(
             '--outfile', nargs='?', type=argparse.FileType('w'),
             default=sys.stdout)
+
+    def classify_office(self, res):
+        for office_re in self.office_list:
+            if re.search(office_re, res[0], flags=re.I):
+                return self.office_list[office_re]
+
+        return 'інше'
 
     def apply_operation(self, res, operation):
         def floatify(val):
@@ -38,6 +76,8 @@ class Command(BaseCommand):
             return sum(map(floatify, res))
         elif flt == "count":
             return len(res)
+        elif flt == "classify_office":
+            return self.classify_office(res)
         elif flt == "count_nonempty":
             return len(list(filter(None, res)))
         elif flt == "join":
