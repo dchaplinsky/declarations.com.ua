fa55ad047d9990d18a6cc8f9204d23ca94ba668c
ArialBlack <andreyturik@gmail.com>
2017-06-04 21:28:13 +0300

    finished with frontpage
    todo: add mobile version

--- declarations_site/catalog/static/js/main.js
+++ declarations_site/catalog/static/js/main.js
@@ -5,8 +5,6 @@ document.addEventListener(
 );
 
 $(function() {
-    var declNews = '';
-
     function setColumnsHeights (list, item) {
         $list = $(list);
         $items = $list.find(item);
@@ -289,6 +287,43 @@ $(function() {
         $('[data-toggle="tooltip"]').tooltip();
     }
 
+    function populateBihusNews(data, $container, ntitle, link) {
+        var title = data[0].title,
+            nid = data[0].nid,
+            teaser_text = data[0].field_teaser_text,
+            created = data[0].created,
+            big_teaser_media = data[0].field_teaser_media_1.replace(/^\s+|\s+$/g,''),
+            declNews = '';
+
+        declNews = declNews + '<div class="top-news"><a class="top-news-card" href="https://bihus.info/node/' + nid +'"><div class="news-block-title">Останні новини ' + ntitle + '</div>';
+        declNews = declNews +  '<div class="top-news-container"><img src="' +  big_teaser_media +'" />';
+        declNews = declNews +  '<div class="top-news-info"><h4>' + title  + '</h4>';
+        declNews = declNews +  '<h6>' + created + '</h6><div class="top-teaser">' + teaser_text + '</div></div>';
+        declNews = declNews + '</div></a></div>';
+
+        declNews = declNews + '<div class="more-news"><div class="more-news-container">';
+
+        for(i = 1; i < 6; i ++) {
+            title = data[i].title,
+                nid = data[i].nid,
+                teaser_text = data[i].field_teaser_text,
+                created = data[i].created,
+                teaser_media = data[i].field_teaser_media;
+
+            declNews = declNews +  '<a class="news-row" title="' + title +'" href="https://bihus.info/node/' + nid +'"><div class="media">';
+            declNews= declNews + '<div class="media-left">';
+            declNews= declNews + '<div class="media-object"><img src="' + teaser_media +'" />';
+            declNews= declNews + '</div></div>';
+            declNews= declNews + '<div class="media-body card-body">';
+            declNews= declNews + '<div class="card-flip"><div class="face front"><h4 class="media-heading">' + title + '</h4>';
+            declNews= declNews + '<h6>' + created + '</h6></div><div class="face back">' + teaser_text +'</div></div>';
+            declNews= declNews + '</div></div></a>';
+        }
+
+        declNews = declNews + '<a href="' + link + '" class="more-news-more-link">Більше новин тут</a></div></div>';
+        $($container).append(declNews);
+    }
+
     function fetchBihusNews() {
         $('body').addClass('ajax-run');
 
@@ -297,30 +332,18 @@ $(function() {
                 type: 'GET'
             })
             .done(function(data) {
-                for(i = 0; i < data.length; i ++) {
-                    console.log(data[i]);
-                    var title = data[i].title,
-                        nid = data[i].nid,
-                        teaset_text = data[i].field_teaser_text,
-                        created = data[i].created,
-                        teaser_media = data[i].field_teaser_media;
+                populateBihusNews(data, '#decl-news-block', 'декларацій', 'https://bihus.info/projects/deklaracii');
+            })
+            .fail(function(event) {
+                console.log(event.status);
+            });
 
-                    //console.log(teaser_media);
-
-                    declNews = declNews +  '<div class="media">';
-                    declNews = declNews +  '<div class="media-left">';
-                    declNews = declNews +  '<a title="' + title +'" href="https://bihus.info/node/' + nid +'">';
-                    declNews = declNews +  '<img class="media-object" src="' + teaser_media +'" alt="' + title +'">';
-                    declNews = declNews +  '</a></div>';
-                    declNews = declNews +  '<div class="media-body">';
-                    declNews = declNews +  '<h4 class="media-heading">' + title + '</h4>';
-                    declNews = declNews +  '<h6>' + created + '</h6><p>' + teaset_text + '</p>';
-                    declNews = declNews +  '</div></div>';
-                }
-
-                aRun = false;
-                //return declNews;
-                
+        $.ajax({
+                url: 'https://bihus.info/restapi/bihus-news',
+                type: 'GET'
+            })
+            .done(function(data) {
+                populateBihusNews(data, '#decl-bihus-block', 'Bihus.info', 'https://bihus.info/news/all');
             })
             .fail(function(event) {
                 console.log(event.status);
@@ -328,13 +351,14 @@ $(function() {
     }
 
     $(document).ajaxStop(function () {
-        if(aRun === false) {
-            $('#bihus-news').append(declNews);
-        }
+        $('body').removeClass('ajax-run');
     });
 
     $(document).ready(function() {
-        fetchBihusNews();
+        if($('.front').length > 0) {
+            fetchBihusNews();
+        }
+
         $.material.init();
         $('[data-toggle="tooltip"]').tooltip();
         setExFormStateFromUrl();
