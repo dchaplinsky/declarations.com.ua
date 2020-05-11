45254964ce9abe8313be8add342e8db44facf784
Anton Zhdanov <anton.s.zhdanov@gmail.com>
2020-05-11 21:00:13 +0300

    Style amends for tooltips

--- declarations_site/catalog/static/js/partials/n-tooltip.js
+++ declarations_site/catalog/static/js/partials/n-tooltip.js
@@ -4,15 +4,25 @@
       $('.n-tooltip__trigger--active').removeClass('n-tooltip__trigger--active');
     }
 
-    $('.n-tooltip__trigger').on('click', function() {
+    $('.n-tooltip__trigger').on('click', function(e) {
+      var target = $(e.target);
+
+      if ((target.closest('.n-tooltip').length || target.hasClass('n-tooltip__close'))) {
+        return;
+      }
+
       closeTooltips();
       $(this).toggleClass('n-tooltip__trigger--active').trigger("tooltip-shown");
     });
 
+    $('.n-tooltip__close').on('click', closeTooltips);
+
     $(document.body).on('click', function handleClickOutside(e) {
+      var target = $(e.target);
+
       if (
-        !$(e.target).closest('.n-tooltip').length &&
-        !$(e.target).closest('.n-tooltip__trigger--active').length
+        (!target.closest('.n-tooltip').length || target.hasClass('n-tooltip__close')) &&
+        !target.closest('.n-tooltip__trigger').length
       ) {
         closeTooltips();
       }
