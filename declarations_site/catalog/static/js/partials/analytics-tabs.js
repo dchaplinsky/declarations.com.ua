a856f5da6c418ec1d9cdcdd97c725d4d1e07c474
Anton Zhdanov <anton.s.zhdanov@gmail.com>
2020-05-27 21:34:29 +0300

    BI: Preserve iframes on switching tabs

--- declarations_site/catalog/static/js/partials/analytics-tabs.js
+++ declarations_site/catalog/static/js/partials/analytics-tabs.js
@@ -3,11 +3,21 @@
     $('.analytics-tabs__tab').on('click', function(e) {
       e.preventDefault();
       var $this = $(this),
-        anchor = $(this).find('.analytics-tabs__tab-name');
+        anchor = $(this).find('.analytics-tabs__tab-name'),
+        iframeSrc = anchor.data('iframe-src'),
+        iframesContainer = $('.analytics-page__iframe'),
+        currentIframe = iframesContainer.find('[src="' + iframeSrc + '"]');
+
       $this.parents('.analytics-tabs').find('.analytics-tabs__tab').removeClass('analytics-tabs__tab--active');
       $this.addClass('analytics-tabs__tab--active');
 
-      $('.analytics-page__iframe iframe').attr('src', anchor.data('iframe-src'));
+      iframesContainer.find('iframe').hide();
+
+      if (currentIframe.length) {
+        currentIframe.show();
+      } else {
+        iframesContainer.append($('<iframe/>').attr('src', iframeSrc));
+      }
     });
   });
 })(jQuery);
